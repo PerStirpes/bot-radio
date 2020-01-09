a33461d59dd548ee33445aeaf74076e75a4fea1e
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2020-01-09 13:08:49 -0800

    add react

---
+++ js-r/hooks.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+export default function useOutsideClick(ref, callback) {
+  const handleClickOutside = event => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  });
+}
